1b5df06688f9fbd2420baf2755943aacacc69611
sarva1210 <sarva.vish77@gmail.com>
2026-01-21 20:17:09 +0530

    js(left side elements)

---
+++ app.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById("canvas");
+
+// left tools
+const shapeSelect = document.getElementById("shape-select");
+const addShapeBtn = document.getElementById("add-shape-btn");
+const addTextBtn = document.getElementById("add-text-btn");
+const snapToggle = document.getElementById("snap-toggle");
+
+// save/dlt/load/rename/select btns
+const designNameInput = document.getElementById("design-name");
+const designList = document.getElementById("design-list");
+const renameBtn = document.getElementById("rename-btn");
+const deleteSaveBtn = document.getElementById("delete-save-btn");
+const saveBtn = document.getElementById("save-btn");
+const loadBtn = document.getElementById("load-btn");
+const clearBtn = document.getElementById("clear-btn");
+
+// export
+const exportJsonBtn = document.getElementById("export-json-btn");
+const exportHtmlBtn = document.getElementById("export-html-btn");
+
+const toolAddShape = document.getElementById("tool-add-shape");
+const toolAddText = document.getElementById("tool-add-text");
+const exportJsonMini = document.getElementById("export-json-mini");
+const exportHtmlMini = document.getElementById("export-html-mini");
